c03c425f4154ce5f911c375e5bd035b2b4afd567
Wen <wenrui@berkeley.edu>
2018-10-19 01:18:21 -0700

    showed non-associativity in matrix-vector operation

--- examples/serial_mat_vect.c
+++ examples/serial_mat_vect.c
@@ -20,6 +20,15 @@ double toc(void){
   return (end.tv_sec - start.tv_sec) + 1.0e-6 * (end.tv_usec - start.tv_usec);
 }
 
+//helper to sum the vector
+double sum_vector(double* vector, int n) {
+  double sum;
+  for (int i = 0; i < n; i++){
+    sum = sum + vector[i];
+  }
+  return sum;
+}
+
 
 //function to shuffle the rows
 void randomize(double** mat, int n) {
@@ -31,6 +40,8 @@ void randomize(double** mat, int n) {
     mat[j] = temp;
   }
 }
+
+//helper to sum the array
 
 
 //simple matrix-vector multiply
@@ -88,53 +99,55 @@ int main(int argc, char** argv){
   double *vector = malloc(n * sizeof(double));  
   double *result = malloc(n * sizeof(double));
 
-  printf("%s\n", "Vector is:");
+  //printf("%s\n", "Vector is:");
   for (int i = 0; i < n; i++) {
     //vector[i] = rand();
-    vector[i] = 1;
-    printf("%0.2f\n", vector[i]);
+    vector[i] = i + 1;
+    //printf("%0.2f\n", vector[i]);
   }
 
-  printf("%s\n", "Original Matrix is:");
+  //printf("%s\n", "Original Matrix is:");
   srand(time(NULL));
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {
       // matrix[i][j] = rand();
       matrix[i][j] = rand();
-      printf("%0.2f     ", matrix[i][j]);
+      //printf("%0.2f     ", matrix[i][j]);
     }
-    printf("\n");
+    //printf("\n");
   }
 
   matrix_vector_mult(matrix, vector, result, n, n);
 
-  printf("%s\n", "Result vector is:");
-  for (int i = 0; i < n; i++) {
-    printf("%0.2f\n", result[i]);
-  }
+  // printf("%s\n", "Result vector is:");
+  // for (int i = 0; i < n; i++) {
+  //   printf("%0.2f\n", result[i]);
+  // }
 
   printf("%s\n", "Original Sum is:");
-  sum = reproBLAS_dsum(n, result, 1);
+  //sum = reproBLAS_dsum(n, result, 1);
+  sum = sum_vector(result,n);
   printf("%g\n", fabs(sum));
 
-  printf("%s\n", "Shuffled Matrix is:");
+  //printf("%s\n", "Shuffled Matrix is:");
   randomize(matrix, n);
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      printf("%0.2f     ", matrix[i][j]);
-    }
-    printf("\n");
-  }  
+  // for (int i = 0; i < n; i++) {
+  //   for (int j = 0; j < n; j++) {
+  //     printf("%0.2f     ", matrix[i][j]);
+  //   }
+  //   printf("\n");
+  // }  
 
   matrix_vector_mult(matrix, vector, result, n, n);
 
-  printf("%s\n", "Shuffled result vector is:");
-  for (int i = 0; i < n; i++) {
-    printf("%0.2f\n", result[i]);
-  }
+  // printf("%s\n", "Shuffled result vector is:");
+  // for (int i = 0; i < n; i++) {
+  //   printf("%0.2f\n", result[i]);
+  // }
 
   printf("%s\n", "Shuffled Sum is:");
-  sum_shuffled = reproBLAS_dsum(n, result, 1);
+  //sum_shuffled = reproBLAS_dsum(n, result, 1);
+  sum_shuffled = sum_vector(result,n);
   printf("%g\n", fabs(sum_shuffled));
 
   printf("%s : %g\n", "Difference is", fabs(sum - sum_shuffled));
